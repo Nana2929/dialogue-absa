348b41492d685d1a740cad60f21a06d90770cc59
Nana2929 <b06102020@ntu.edu.tw>
2023-05-24 01:58:42 +0800

    add: fix log name

--- run_train.sh
+++ run_train.sh
@@ -1,5 +1,6 @@
 
 exp_config_name='diaasq-t5-speaker-spec-en'
 log_dir='logs'
+time_stamp=$(date "+%Y.%m.%d-%H.%M.%S")
 python train_t5_diaasq_speaker_spec.py \
-    -cfg=configs/${exp_config_name}.yaml > ${log_dir}/${exp_config_name}.log
+    -cfg=configs/${exp_config_name}.yaml > ${log_dir}/${exp_config_name}${time_stamp}.log
