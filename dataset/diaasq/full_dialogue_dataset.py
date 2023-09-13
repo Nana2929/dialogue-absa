da384567478e84fa395df8a8519e22ef21f7e68f
Nana2929 <b06102020@ntu.edu.tw>
2023-09-13 12:15:21 +0800

    add: converted diaasq (en) and scripts

--- dataset/diaasq/full_dialogue_dataset.py
+++ dataset/diaasq/full_dialogue_dataset.py
@@ -1,165 +1,227 @@
 import os
-from typing import List, Tuple
+from collections import defaultdict
 import numpy as np
 import logging
 
-from dataset.utils import *
-from dataset.diaasq.base import BaseDiaAsqDataset
-from dataset.constants import diaasq_instruct_terms as instruct_terms
-# need yaml config to determine
+import sys
 
-def add_speaker_prefix(data: List[Dict]):
+
+from dataset.utils import *
+from dataset.constants import diaasq_instruct_terms as instruct_terms
+
+# need yaml config to determine
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def add_speaker_prefix(data: list[dict]) -> tuple[list[dict], set[str]]:
     # check number of speakers
-    speakers = data['speakers']
-    n_speaker = len(set(speakers))
-    sentences = data['sentences'].copy()
+    speakers = data["speakers"]
+    sentences = data["sentences"].copy()
     sp_sent_pairs = zip(speakers, sentences)
     speaker_list = set()
     for i, (sp, sent) in enumerate(sp_sent_pairs):
         sp = number_to_char(sp)
         speaker_list.add(sp)
-        sent = f'{sp}: {sent}'
+        sent = f"{sp}: {sent}"
         sentences[i] = sent
     data_copy = data.copy()
     del data
-    data_copy['sentences'] = sentences
+    data_copy["sentences"] = sentences
     return data_copy, speaker_list
-class FullDiaAsqDataset(BaseDiaAsqDataset):
 
-    def __init__(self,
-                 src,
-                 data_root: os.PathLike,
-                 train_split_name: str,
-                 test_split_name: str,
-                 k: int,
-                 seed: int,
-                 prompt_path=os.PathLike):
-        super().__init__(src, data_root, train_split_name, test_split_name)
-        self.k = k
-        self.seed = seed
-        self.data = self.read_json(self.test_filepath)
-        self.train_data = self.read_json(self.train_file_path)
-        self.prompt_prefix = self._load_instruction(prompt_path)
+
+pol_map = {
+    "pos": "positive",
+    "neg": "negative",
+    "other": "neutral",
+    "neu": "neutral",
+}
+
+
+# ==========================
+class FullDiaAsqDataset:
+    def __init__(
+        self,
+        src,
+        data_path: os.PathLike,
+        instruct_path=os.PathLike,
+    ):
+        self.src = src
+        self.data_path = data_path
+        self.data = load_json(self.data_path)
+        self.instruct_prefix = self._load_instruction(instruct_path)
         self.instruct_terms = instruct_terms[self.src]
 
-        np.random.seed(self.seed)
-
-    def __getitem__(self, idx) -> Tuple[any]:
+    def __getitem__(self, idx) -> tuple[any]:
         # test sample
         sample = self.data[idx]
-        defs, examples, test_sample = self._make_prompt(sample, self.k)
+        input_string, output_string, success_count = self._form_output(sample)
+        char_speakers = [number_to_char(sp) for sp in sample["speakers"]]
+        doc_id = sample["doc_id"]
+        extra_metadata = {
+            "success_quad_count": success_count,
+            "quad_count": len(sample["triplets"]),
+        }
 
-        # return string, full_sample (original data + answer)
-        return defs + '\n' + examples + '\n' + test_sample, sample
+        return {
+            "instruction": self.instruct_prefix,
+            "input": input_string,
+            "output": output_string,
+            "doc_id": doc_id,
+            "char_speakers": char_speakers,
+            "metadata": get_metadata(sample) | extra_metadata,
+        }
 
-    def _load_instruction(self, path: os.PathLike):
+    def _load_instruction(self, path: os.PathLike) -> str:
         """
-        load instruction
+        LLaMA has <SystemPrompt>, <Instruction>, <Input>, <Output> (for demo) parts as its total input.
+        load instruction, to be placed at `instruction`
         """
         assert os.path.exists(path)
-        with open(path, 'r') as f:
-            prompt_prefix = f.read()
-        return prompt_prefix
+        with open(path, "r") as f:
+            instruct_prefix = f.read()
+        logger.info(f"Loaded instruction from {path}.")
+        return instruct_prefix
 
-    def _make_prompt(self, test_data: Dict[str, any], k: int) -> Tuple[str, str, str]:
-        """The prompt roughly follows Yizhong Wang et al. (2022)'s paper
-        Super-NaturalInstructions:
-        Generalization via Declarative Instructions on 1600+ NLP Tasks.
-        - Definitions
-        - Examples (in paper, 1 positive, 1 negative)
-        - Evaluation Instance
+    def __get_sentence_boundaries(self, raw_sentences) -> list[tuple[int, int]]:
         """
-        definitions = self.prompt_prefix
-        k_shots = self._k_shot(k)
-        np.random.shuffle(k_shots)  # order randomize
-        ordered_k_shots = [
-            self._form_example(self.train_data[id]) for id in k_shots
-        ]
-        ordered_k_shots_string = '\n'.join(ordered_k_shots)
-        test_sample = self._form_example(test_data, with_ans=False)
-        return definitions, ordered_k_shots_string, test_sample
+        Parameters
+        ----------
+        raw_sentences : _type_
+            The DiaASQ dataset's original key 'sentences'
+        Returns
+        -------
+        list(tuple(int, int))
+            A list of start and end indices of each sentence when sentences are
+            concatenated as " ".join(raw_sentences)
+        """
+        sentence_indices = []
+        start_index = 0
+        for sent in raw_sentences:
+            end_index = start_index + len(sent.split())
+            sentence_indices.append((start_index, end_index))
+            start_index = end_index
+        return sentence_indices
 
-    def _find_legal_pool(self, ) -> List[int]:
-        # first check if the attribute exists in this class
-        if hasattr(self, '_legal_pool'):
-            return self._legal_pool
-        legal_pool = []
-        for id, d in enumerate(self.train_data):
-            senti_set = set()
-            triplets = d['triplets']
-            for triplet in triplets:
-                pol = triplet[6]
-                senti_set.add(pol)
-            if len(senti_set) == 3:
-                legal_pool.append(id)
-        self._legal_pool = legal_pool
-        # count legal pool
-        logger.info(f'Legal pool size: {len(legal_pool)}')
-        return legal_pool
+    def __get_opn_holder(
+        self,
+        opn_start_index: int,
+        opn_end_index: int,
+        sentence_indices: list[str],
+        speakers: list[int],
+    ) -> str:
+        """
 
-    def _form_example(self, data: Dict[str, any], with_ans: bool=True):
+        Parameters
+        ----------
+        opn_start_index : int
+        opn_end_index : int
+        sentence_indices : list[str]
+        speakers : list[int]
+
+        Returns
+        -------
+        str
+            The speaker of the sentence that contains the opinion spans in char form, e.g. 'A', 'B', 'C', etc.
+
+        Raises
+        ------
+        ValueError
+            If the sentence that contains the opinion spans cannot be found.
+        """
+        opn_sent_idx = None
+        for sent_id, (s, t) in enumerate(sentence_indices):
+            if opn_start_index >= s and opn_end_index < t:
+                opn_sent_idx = sent_id
+                break
+        if opn_sent_idx is None:
+            raise ValueError(
+                f"Cannot find the sentence (therefore, holder) that contains the opinion spans."
+            )
+        opn_holder = speakers[opn_sent_idx]
+        # holder as a char
+        return number_to_char(opn_holder)
+
+    def _form_output(self, data: Dict[str, any]) -> tuple[str, str, int]:
+        from collections import namedtuple
+
+        SentTuple = namedtuple(
+            "SentTuple", ["opn_holder", "target", "aspect", "opn", "pol", "opn_start"]
+        )
         """
         formulate 1 example.
         Parameters
         ----------
         data: Dict[str]
             data from json file
-        with_ans: bool
-            whether to include answer triplets or not.
-            if True, return example with answer triplets, normally used for in-context examples.
-            else, return example without answer triplets, normally used for evaluation/prompting for answer.
-        """
-        _data = data.copy()
-        if self.src == 'zh':
-            _data = remove_space(data)  # only for zh
+        Returns
+        -------
+        tuple[str, str, int]
+            input string, output string, number of quadruples successfully converted
 
-        _data, _= add_speaker_prefix(_data)
-        preproc_sentences = _data['sentences']
-        quads = _data['triplets']
-        quad_string = ''
+        """
+        original_sentences = data["sentences"]
+        speakers = data["speakers"]
+        #  =========================
+        _data = data.copy()
+        if self.src == "zh":
+            _data = remove_space(data)  # only for zh
+        _data, _ = add_speaker_prefix(_data)
+        preproc_sentences = _data["sentences"]
+        # !!! Concatenate sentences with space
+        sent_string = " ".join(preproc_sentences)
+        quads = _data["triplets"]
+        # ===========================
+        sentence_indices = self.__get_sentence_boundaries(original_sentences)
+
+        quad_strings = []
         for quad in quads:
             pol = quad[6]
             target_string = quad[7]
             aspect_string = quad[8]
             opn_string = quad[9]
+            opn_start_index, opn_end_index = quad[4], quad[5]
+            try:
+                opn_holder = self.__get_opn_holder(
+                    opn_start_index, opn_end_index, sentence_indices, speakers
+                )
+            except ValueError as e:
+                # drop this quad
+                logger.warning("Dropping quadruple due to error: %s" % e)
+                continue
             # fill with null if empty
-            if target_string == '': target_string = 'null'
-            if aspect_string == '': aspect_string = 'null'
-            if opn_string == '': opn_string = 'null'
+            if target_string.strip() == "":
+                target_string = "null"
+            if aspect_string.strip() == "":
+                aspect_string = "null"
+            if opn_string.strip() == "":
+                opn_string = "null"
 
-            formatter = '(%s,%s,%s,%s)'
-            quad_string += formatter % (target_string, aspect_string,
-                                        opn_string, pol) + '\n'
+            sent_tuple = SentTuple(
+                opn_holder,
+                target_string,
+                aspect_string,
+                opn_string,
+                pol,
+                opn_start_index,
+            )
+            quad_strings.append(sent_tuple)
+        formatter = "%s: (%s, %s, %s, %s)"
+        # !!! Sort by opn_start_index (the appearance order in the dialogue)
+        quad_strings = sorted(quad_strings, key=lambda x: x.opn_start)
+        quad_string = " ".join(
+            [
+                formatter
+                % (
+                    quad.opn_holder,
+                    quad.target,
+                    quad.aspect,
+                    quad.opn,
+                    pol_map[quad.pol],
+                )
+                for quad in quad_strings
+            ]
+        )
 
-        sent_string = '\n'.join(preproc_sentences)
-        interm = self.instruct_terms['input']
-        outterm = self.instruct_terms['output']
-        if with_ans:
-            return interm + sent_string + '\n\n' + outterm + quad_string
-        # prompting for answer
-
-        return interm + sent_string + '\n\n' + outterm
-
-    def _k_shot(self, k: int) -> List[int]:
-        # sample k unique samples from legal pool
-        legal_pool = self._find_legal_pool()
-        assert len(legal_pool) >= k, f'Legal pool size {len(legal_pool)} < k {k}'
-        k_shot = np.random.choice(legal_pool, k,
-                                  replace=False)  # replace ~= repetition
-
-        return k_shot
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    # data_root = './data/diaasq/dataset'
-    # train_split_name = 'train'
-    # test_split_name = 'valid'
-    # k = 3
-    # prompt_path = f'./prompt/experiment/diaasq-fulldiag'
-    # dataset = FullDiaAsqDataset('en', data_root, train_split_name,
-    #                             test_split_name, k, 0, prompt_path)
-    # input_string, full_sample = dataset[0]
-    # print(full_sample)
-    # print(input_string)
+        return sent_string, quad_string, len(quad_strings)
--- dataset/utils.py
+++ dataset/utils.py
@@ -1,40 +1,129 @@
 from typing import List, Dict
+from collections import defaultdict
 from copy import deepcopy
 import pickle
 import json
 
 
-
-
 def load_pkl(path: str):
-
-    with open(path, 'rb') as f:
+    with open(path, "rb") as f:
         data = pickle.load(f)
     return data
 
-def load_json(path: str) -> List[Dict]:
 
-    with open(path, 'r') as f:
+def load_json(path: str) -> list[dict]:
+    with open(path, "r") as f:
         data = json.load(f)
     return data
 
-def remove_space(example: List[Dict]):
+
+def save_json(data: list[dict], path: str):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=2)
+
+
+def remove_space(example: dict) -> dict:
     _example = deepcopy(example)
-    sentences = _example['sentences']
+    sentences = _example["sentences"]
     for i, sent in enumerate(sentences):
         sentences[i] = _remove_space(sent)
     return _example
 
+
 def _remove_space(text):
     # such that the texts look more natural
     text = text.split()
-    text = ''.join(text)
+    text = "".join(text)
     return text
 
+
 def char_to_number(char: str):
-    assert char < 'Z', 'char must be in [A, Z]'
-    return ord(char) - ord('A')
+    assert char < "Z", "char must be in [A, Z]"
+    return ord(char) - ord("A")
+
 
 def number_to_char(number: int):
-    assert number < 26, 'number must be in [0, 25] such that it can be converted to a char'
-    return chr(number + ord('A'))
+    assert (
+        number < 26
+    ), "number must be in [0, 25] such that it can be converted to a char"
+    return chr(number + ord("A"))
+
+
+def get_metadata(data: Dict[str, any]) -> Dict[str, any]:
+    # number of aspects, repeated aspects
+    # number of entities, repeated entities
+    # number of entity-aspect pairs, repeated entity-aspect pairs
+    # aspect count
+    # entity count
+    # entity-aspect count
+
+    aspect_count_dict = defaultdict(int)
+    entity_count_dict = defaultdict(int)
+    entity_aspect_count_dict = defaultdict(int)
+    gold_aspects = [a[-1] for a in data["aspects"]]
+    gold_entities = [e[-1] for e in data["targets"]]
+
+    gold_ent_aspects = []
+    for tup in data["triplets"]:
+        gold_ent_aspects.append((tup[7], tup[8]))
+
+    for aspect in gold_aspects:
+        if aspect.strip() == "":
+            continue
+        aspect_count_dict[aspect] += 1
+    for entity in gold_entities:
+        if entity.strip() == "":
+            continue
+        entity_count_dict[entity] += 1
+
+    for ent, asp in gold_ent_aspects:
+        if ent.strip() == "" or asp.strip() == "":
+            continue
+        entity_aspect_count_dict[(ent, asp)] += 1
+
+    # =================================================
+    # pos, neg, neu count for the full dialogue, regardless of entity/aspect
+    # pos, neg, neu count for each entity
+    # pos, neg, neu count for each aspect
+    entity_senti_number_dict = defaultdict(lambda: defaultdict(int))
+    aspect_senti_number_dict = defaultdict(lambda: defaultdict(int))
+    for tup in data["triplets"]:
+        entity = tup[7]
+        senti = tup[6]
+        if entity.strip() == "":
+            continue
+        entity_senti_number_dict[entity][senti] += 1
+
+        aspect = tup[8]
+        if aspect.strip() == "":
+            continue
+        aspect_senti_number_dict[aspect][senti] += 1
+    repeated_entity_aspect_count = sum(
+        [v for v in entity_aspect_count_dict.values() if v > 1]
+    )
+    if repeated_entity_aspect_count > 0:
+        entity_aspect_senti_number_dict = defaultdict(lambda: defaultdict(int))
+        for tup in data["triplets"]:
+            entity = tup[7]
+            aspect = tup[8]
+            senti = tup[6]
+            if aspect.strip() == "" or entity.strip() == "":
+                continue
+            # use f"{entity}_{aspect}"
+            # to avoid TypeError: keys must be str, int, float, bool or None, not tuple when saving to json
+            entity_aspect_senti_number_dict[f"{entity}_{aspect}"][senti] += 1
+
+    # =================================================
+    metadata = {
+        "aspect_count": len(aspect_count_dict),
+        "entity_count": len(entity_count_dict),
+        "entity_aspect_count": len(entity_aspect_count_dict),
+        "repeated_aspect_count": sum([v for v in aspect_count_dict.values() if v > 1]),
+        "repeated_entity_count": sum([v for v in entity_count_dict.values() if v > 1]),
+        "repeated_entity_aspect_count": repeated_entity_aspect_count,
+        "entity_senti_number_dict": entity_senti_number_dict,
+        "aspect_senti_number_dict": aspect_senti_number_dict,
+    }
+    if repeated_entity_aspect_count > 0:
+        metadata["entity_aspect_senti_number_dict"] = entity_aspect_senti_number_dict
+    return metadata
